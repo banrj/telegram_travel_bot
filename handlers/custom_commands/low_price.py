0d71cb0b13c420ae14724628f3417636c9cef6b9
wallbreak <eh.ehr@bk.ru>
2022-06-02 15:02:59 +0200

    create commands lowprice and highprice

--- handlers/custom_commands/low_price.py
+++ handlers/custom_commands/low_price.py
@@ -5,3 +5,4 @@ from telebot.types import Message
 @bot.message_handler(commands=['lowprice'])
 def answer_low_price(message: Message) -> None:
     bot.send_message(message.chat.id, 'Команда еще не готова')
+    bot.send_message(message.chat.id, 'Команда еще не готова')
